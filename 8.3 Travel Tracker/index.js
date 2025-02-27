b1cc0b0163d0715218c37da15088b5e4be1070b6
jivatech <jeevakbs@gmail.com>
2025-02-27 20:58:03 +0530

    polished the code to make it user friendly

--- 8.3 Travel Tracker/index.js
+++ 8.3 Travel Tracker/index.js
@@ -38,35 +38,57 @@ app.get("/", async (req, res) => {
   // });
   const countries = await checkVisisted();
   res.render("index.ejs", {total:countries.length, countries:countries});
-  console.log(countries);
 });
 
 app.post("/add", async (req, res)=>{
   const input = req.body.country;
   const inputUpperCase = input.toUpperCase();
-  try {
-    const getCountryCode = await db.query('SELECT country_code FROM countries WHERE country_name= $1', [input]);
- 
-
-  // console.log("From DB we getting this." + getCountryCode.rows);
-  // getCountryCode.rows.forEach(countryCD => {
-  //   console.log("Right Country Code: " + countryCD.country_code);
-  // });
-
-  if (input.length === 2){
-    await db.query('INSERT INTO visited_countries (country_code) VALUES($1)', [inputUpperCase]);
-  }else if(getCountryCode.rows.length !== 0){
-    const countryCode = getCountryCode.rows[0].country_code;
-    await db.query('INSERT INTO visited_countries (country_code) VALUES($1)', [countryCode]);
-  }
-  res.redirect("/");
-  } catch (error) {
-  const countries = await visitedCountries();
-  res.render("index.ejs", {
-    countries: countries,
-    total: countries.length,
-    error:"The country you entered is not exists, please again."
-  });
+  if (inputUpperCase.length === 2){
+    try {
+      await db.query('INSERT INTO visited_countries (country_code) VALUES($1)', [inputUpperCase]);
+      res.redirect("/");
+    } catch (error) {
+      console.log("Problem will inserting data for length 2: " + error);
+      const countries = await checkVisisted();
+      res.render("index.ejs", {
+        countries: countries,
+        total: countries.length,
+        error:error
+      });
+    }
+    
+  } else {
+    try {
+      const getCountryCode = await db.query("SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%'", [input.toLowerCase()]);
+      // console.log("From DB we getting this." + getCountryCode.rows);
+      // getCountryCode.rows.forEach(countryCD => {
+      //   console.log("Right Country Code: " + countryCD.country_code);
+      // });
+      const data = getCountryCode.rows[0];
+      const countryCode = data.country_code;
+    try {
+      if(data.length !== 0){
+        //const countryCode = getCountryCode.rows[0].country_code;
+        await db.query('INSERT INTO visited_countries (country_code) VALUES($1)', [countryCode]);
+      }
+      res.redirect("/");
+    } catch (error) {
+      const countries = await checkVisisted();
+      res.render("index.ejs", {
+        countries: countries,
+        total: countries.length,
+        error: "Country has already been added, try again"
+      });
+    }
+    
+    } catch (err) {
+      const countries = await checkVisisted();
+      res.render("index.ejs", {
+        countries: countries,
+        total: countries.length,
+        error:"The country you entered is not exists, please try again."
+      });
+    }
   }
 });
 
